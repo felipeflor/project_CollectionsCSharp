90078b088760054a461f3a9ab81d25555cd994f4
Felipe <felipedossantosflor@gmail.com>
2022-07-21 17:14:45 -0300

    Exercicio

---
+++ CList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Lista");
+
+            List<string> lista = new List<string>();
+            lista.Add("Matheus");
+            lista.Add("Maju");
+            lista.Add("Felipe");
+
+            lista.Insert(1, "Rafel");
+            lista.Remove("Maju");
+            lista.RemoveAt(2);
+
+            foreach(string nome in lista)
+            {
+                Console.WriteLine($"Aqui está a Pessoa: {nome}");
+            }
+
+        }
+    }
+}
---
+++ CQueue/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Fila");
+
+            Queue<Pessoa> pessoas = new Queue<Pessoa>();
+
+            pessoas.Enqueue(new Pessoa("Jesus", "Las Vegas", 33));
+            pessoas.Enqueue(new Pessoa("Israel", "Jerusalém", 45));
+            pessoas.Enqueue(new Pessoa("Jesus", "Amsterdan", 25));
+
+            Console.WriteLine($"O primeiro da fila é: {pessoas.Dequeue()}");
+
+            pessoas.Peek();
+
+            Console.WriteLine($"Pessoas na fila: {pessoas.Count}");
+
+            foreach(Pessoa pessoa in pessoas){
+                Console.WriteLine(pessoas.ToString());
+            }
+
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string Cidade { get; set; }
+            public int Idade { get; set; }
+
+            public Pessoa(string nome, string cidade, int idade)
+            {
+                Nome = nome;
+                Cidade = cidade;
+                Idade = idade;
+            }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome} | Cidade: {Cidade} | Idade: {Idade}";
+            }
+
+
+        }
+
+    }
+}
