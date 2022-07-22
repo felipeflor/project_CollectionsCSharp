d8b592322c3177f74f601a2c969cb3c2676ccb3e
Felipe <felipedossantosflor@gmail.com>
2022-07-22 10:12:40 -0300

    Continuação aula Collections

--- CDictionary/Program.cs
+++ CDictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static CQueue.Program;
 
 namespace CDictionary
 {
@@ -6,7 +8,19 @@ namespace CDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Dicionário!");
+
+            Dictionary<string, Pessoa> dic = new Dictionary<string, Pessoa>();
+
+            dic.Add("Tuti", new Pessoa("Maju", "São Paulo", 35));
+            dic.Add("Jepam", new Pessoa("Jean", "Macedonia", 20));
+            dic.Add("Cupim", new Pessoa("Felipe", "Coimbra", 30));
+
+            foreach(KeyValuePair<string, Pessoa> kvp in dic)
+            {
+                Console.WriteLine($"Chave = {kvp.Key} | Valor = {kvp.Value}");
+            }
+
         }
     }
 }
--- CStack/Program.cs
+++ CStack/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using static CQueue.Program;
 
 namespace CStack
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Pilha");
+
+            Stack<Pessoa> pilhaPessoas = new Stack<Pessoa>();
+
+            pilhaPessoas.Push(new Pessoa("Fabio", "Bruxelas", 36));
+            pilhaPessoas.Push(new Pessoa("Amanda", "Rio", 49));
+            pilhaPessoas.Push(new Pessoa("Thaina", "Barcelona", 12));
+            pilhaPessoas.Push(new Pessoa("Gabriel", "Tokio", 30));
+
+            Console.WriteLine($"Quantidade de pessoas: {pilhaPessoas.Count}");
+
+            Console.WriteLine($"Primeira pessoa do topo: {pilhaPessoas.Peek()}");
+
+            pilhaPessoas.Pop();
+
+            Console.WriteLine($"Primeira pessoa do topo: {pilhaPessoas.Peek()}");
+
+            foreach (Pessoa pessoa in pilhaPessoas)
+            {
+                Console.WriteLine($"{pessoa.ToString()}");
+            }
         }
     }
 }
